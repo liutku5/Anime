82a44efa09683b037df012099881019ac5a3c61f
liutku5 <l1utkuz20@gmail.com>
2024-07-23 11:37:19 +0300

    full

--- src/main/java/com/example/Anime/models/Anime.java
+++ src/main/java/com/example/Anime/models/Anime.java
@@ -3,6 +3,8 @@ package com.example.Anime.models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "anime")
 @Data
@@ -14,20 +16,21 @@ public class Anime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     @Column(name = "id")
     private long id;
 
     @Column(name = "name_jp")
-    private String name_jp;
+    private String nameJP;
 
     @Column(name = "name_en")
-    private String name_en;
+    private String nameEN;
 
     @Column(name = "release_date")
-    private long release_date;
+    private LocalDate releaseDate;
 
     @Column(name = "source_id")
-    private long source_id;
+    private long sourceId;
 
     @Column(name = "episodes")
     private int episodes;
